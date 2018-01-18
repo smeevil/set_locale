da9ae029642bc0fbd9212c2aaf86c0adca70c084
Doc Ritezel <doc@ministryofvelocity.com>
2018-01-17 18:29:18 -0800

    Bump Gettext version

--- mix.exs
+++ mix.exs
@@ -35,7 +35,7 @@ defmodule SetLocale.Mixfile do
   defp deps do
     [
       {:phoenix, "~> 1.3.0"},
-      {:gettext, "~> 0.13.1"},
+      {:gettext, "~> 0.14.0"},
       {:earmark, "~>1.2.0", only: :dev},
       {:ex_doc, "~>0.16.4", only: :dev},
       {:excoveralls, "~> 0.7.3", only: :test},
