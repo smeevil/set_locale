736507f534990f272eeb0f6f5003d3a51ec4051f
Mihai Târnovan <mihai.tarnovan@gmail.com>
2019-02-19 18:50:10 +0200

    Relax deps for Phoenix  and gettext
    Allow Phoenix 1.3 and 1.4, gettext > 0.13.1
    Tests passing

--- mix.exs
+++ mix.exs
@@ -34,10 +34,10 @@ defmodule SetLocale.Mixfile do
 
   defp deps do
     [
-      {:phoenix, "~> 1.3.0"},
-      {:gettext, "~> 0.13.1"},
+      {:phoenix, "~>1.3.0 or ~>1.4.0"},
+      {:gettext, "~>0.14"},
       {:earmark, "~>1.2.0", only: :dev},
-      {:ex_doc, "~>0.16.4", only: :dev},
+      {:ex_doc, ">0.13.1", only: :dev},
       {:excoveralls, "~> 0.7.3", only: :test},
     ]
   end
